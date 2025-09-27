d5ae22516718f1fa8f92952e77a98cf30dd22e7b
Dhurkesh-R <rdhurkesh8@gmail.com>
2025-09-27 11:36:13 +0800

    Notifications

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,8 @@
-import React, { useEffect, useState } from "react";
-import { fetchForecast, fetchHistorical } from "./services/api";
+import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 import Login from "./components/Login";
 import Register from "./components/Register";
 import Dashboard from "./Dashboard";
@@ -8,25 +10,29 @@ import PrivateRoute from "./components/PrivateRoute";
 import { AuthProvider } from "./contexts/AuthContext";
 import { useTheme } from "./contexts/ThemeContext";
 
-
 const App = () => {
-  const { theme, cycleTheme } = useTheme();
+  const { theme } = useTheme();
 
   return (
-      <Router>
-        <AuthProvider>
-          <Routes>
-            <Route path="/login" element={<Login theme={theme}/>} />
-            <Route path="/register" element={<Register />} />
-            <Route path="/" element={
+    <Router>
+      <AuthProvider>
+        <Routes>
+          <Route path="/login" element={<Login theme={theme} />} />
+          <Route path="/register" element={<Register />} />
+          <Route
+            path="/"
+            element={
               <PrivateRoute>
                 <Dashboard />
               </PrivateRoute>
-            } />
-            <ToastContainer position="top-right" autoClose={5000} />
-          </Routes>
-        </AuthProvider>
-      </Router>
+            }
+          />
+        </Routes>
+
+        {/* ✅ Keep ToastContainer outside Routes */}
+        <ToastContainer position="top-right" autoClose={5000} />
+      </AuthProvider>
+    </Router>
   );
 };
 
